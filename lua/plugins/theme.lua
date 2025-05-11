e7360956389db36ad21ed2f7870e18af9ef9ffa7
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2025-05-11 18:47:52 -0300

    cambios a theme, se rompio mason

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,16 +1,16 @@
 return {
-  "alexxGmZ/e-ink.nvim",
-  priority = 1000,
-  config = function()
-    require("e-ink").setup()
-    vim.cmd.colorscheme("e-ink")
 
-    -- choose light mode or dark mode
-    vim.opt.background = "dark"
-    -- vim.opt.background = "light"
-
-    -- or do
-    -- :set background=dark
-    -- :set background=light
-  end,
+  -- add gruvbox
+  { "EdenEast/nightfox.nvim" }, -- lazy
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
+      -- colorscheme = "base16-windows-95",
+      -- colorscheme = "kanagawa",
+      -- colorscheme = "dosbox",
+      -- colorscheme = "carbonfox",
+      -- colorscheme = "monoglow",
+    },
+  },
 }
